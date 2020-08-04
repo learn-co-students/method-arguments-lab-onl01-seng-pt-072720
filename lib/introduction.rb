9bf3be8ce994dc0477c7f018e1bc3978ba3cddc2
justin escobar <escobar.alan@ymail.com>
2020-08-04 21:20:47 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,13 @@
+require 'pry'
+
+def introduction(name)
+  puts "Hi, my name is #{name}."
+  puts "Hi, my name is " + name.to_s + "."
+  #name will serve as a local variable inside of our method body
+end 
+
+def introduction_with_language("Dan","ember.js" )
+   puts name + ',' + language 
+end 
+
+binding.pry 
